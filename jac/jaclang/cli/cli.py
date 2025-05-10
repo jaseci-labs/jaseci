a22087c30f4c28a4c8ed3d54897f4a2c4c77a40d
jayanaka-98 <jayanaka@umich.edu>
2025-05-10 16:49:38 -0400

    spawn thread at jac_run --gins

--- jac/jaclang/cli/cli.py
+++ jac/jaclang/cli/cli.py
@@ -109,6 +109,7 @@ def run(
     session: str = "",
     main: bool = True,
     cache: bool = True,
+    gins: bool = False,
 ) -> None:
     """Run the specified .jac file.
 
@@ -129,6 +130,8 @@ def run(
     # if no session specified, check if it was defined when starting the command shell
     # otherwise default to jaclang.session
     base, mod, mach = proc_file_sess(filename, session)
+    if gins:
+        Jac.attach_gins(mach=mach)
 
     if filename.endswith(".jac"):
         try:
