1b94d2ef4ec8b2cc1360b5d3c785be5f5608621f
ashishmahendra <ashishmahendra@live.com>
2025-06-13 11:55:30 +0000

    Format: updated the has_arch format

--- jac/jaclang/compiler/passes/tool/doc_ir_gen_pass.py
+++ jac/jaclang/compiler/passes/tool/doc_ir_gen_pass.py
@@ -511,6 +511,8 @@ class DocIRGenPass(UniPass):
             if isinstance(i, uni.Token) and i.name == Tok.LBRACE:
                 parts.append(self.tight_line())
                 parts.append(i.gen.doc_ir)
+            elif isinstance(i, uni.Token) and i.name == Tok.RBRACE:
+                parts.append(i.gen.doc_ir)
             else:
                 parts.append(i.gen.doc_ir)
                 parts.append(self.space())
@@ -538,6 +540,7 @@ class DocIRGenPass(UniPass):
                 parts.append(i.gen.doc_ir)
             else:
                 parts.append(i.gen.doc_ir)
+        # parts.pop()
         node.gen.doc_ir = self.group(self.concat(parts))
 
     def exit_arch_has(self, node: uni.ArchHas) -> None:
@@ -550,7 +553,10 @@ class DocIRGenPass(UniPass):
             elif isinstance(i, uni.Token) and i.name == Tok.SEMI:
                 parts.pop()
                 parts.append(i.gen.doc_ir)
-                parts.append(self.space())
+            elif isinstance(i, uni.Token) and i.name == Tok.COMMA:
+                parts.pop()
+                parts.append(i.gen.doc_ir)
+                parts.append(self.indent(self.hard_line()))
             else:
                 parts.append(i.gen.doc_ir)
                 parts.append(self.space())
